3481cc07ff07c259288733e9a626bc52997a627b
ayush.vasu <ayush.vasu@impetus.co.in>
2018-07-20 15:46:16 +0530

    Cahges in Benchmark program and sample output file

--- eth-jdbc-examples/src/main/java/com/impetus/blkchn/eth/BenchMarkEthDriver.java
+++ eth-jdbc-examples/src/main/java/com/impetus/blkchn/eth/BenchMarkEthDriver.java
@@ -6,13 +6,16 @@ import org.web3j.protocol.core.methods.response.EthBlock;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.protocol.ipc.WindowsIpcService;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.math.BigInteger;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.*;
+import java.util.logging.LogManager;
 
 
 public class BenchMarkEthDriver {
@@ -23,7 +26,7 @@ public class BenchMarkEthDriver {
     static final int numIter = 3;
     static Connection conn = null;
     static Random randomNum = null;
-    static String ipPort = "172.25.41.52:8545";
+    static String ipPort = "127.0.0.1:8545";
     static String httpUrl = "http://"+ipPort;
     static String url = "jdbc:blkchn:ethereum://"+ipPort;
     static Web3j web3jClient = null;
@@ -33,25 +36,30 @@ public class BenchMarkEthDriver {
         web3jClient = Web3j.build(new HttpService(httpUrl));
         String driverClass = "com.impetus.eth.jdbc.EthDriver";
 
+        PrintStream out = new PrintStream(new FileOutputStream("Benchmark.txt"));
+        System.setOut(out);
+
         Class.forName(driverClass);
         conn = DriverManager.getConnection(url, null);
         randomNum = new Random();
+        String format = "|%1$-30s|%2$-30s|%3$-30s|\n";
 
         for (int ittr = 0; ittr < numIter; ittr++) {
             List<HashMap> queryList = getQueryList();
             for (Object qmap : queryList) {
                 System.out.println("\n\n");
                 for (Object qtype : ((Map) qmap).keySet()) {
-                    System.out.println("\n"+qtype + " for iteration " + (ittr + 1));
+                    System.out.println("\n-"+qtype);
+                    System.out.println("    -Iteration " + (ittr + 1));
                     int i = 0;
+                    System.out.format(format, "Block Range", "Time By Driver(ms)", "Time By WEB3j(ms)");
                     for (Object query : (List) ((HashMap) qmap).get((String) qtype)) {
-                        System.out.println("\n\t=========================== Block Range of :: "+blockSize[i]+" Blocks===================================");
-                        //System.out.println(blockSize[i++]+"\t\t"+query);
-                        runQuery((String) query);
-                        gtTimeWithWeb3J(blockSize[i]);
+                        String driverTime = String.valueOf(runQuery((String) query));
+                        String web3jTime = String.valueOf(gtTimeWithWeb3J(blockSize[i]));
+                        System.out.format(format,blockSize[i],driverTime,web3jTime);
                         i++;
                     }
-                    System.out.println("\n\n");
+                    System.out.println("==============================================================================================\n\n");
                 }
             }
         }
@@ -68,7 +76,6 @@ public class BenchMarkEthDriver {
             blockRange blnum = getRandomRange(i);
             ((List) queryMap.get("Simple Query")).add("select * from block where blocknumber > " + blnum.rmin + " and blocknumber < " + blnum.rmax);
         }
-
         String qO1 = "select * from block where blocknumber = " + getRandomNumber() + "  order by hash";//1
         queryMap.put("Order By Query", new ArrayList<String>());
         ((List) queryMap.get("Order By Query")).add(qO1);
@@ -100,76 +107,67 @@ public class BenchMarkEthDriver {
         return min + randomNum.nextInt(max);
     }
 
-    public static void runQuery(String q) {
+    public static long runQuery(String q) {
         try {
             Statement stmt = conn.createStatement();
             Long start = System.currentTimeMillis();
             ResultSet rs1 = stmt.executeQuery(q);
             Long end = System.currentTimeMillis();
             while (rs1.next()) {
-                //System.out.println(rs1.toString());
                 break;
             }
             Long qt = (end - start);
-            //System.out.println("\t\tStart Time :: "+start+" end Time :: "+end+" ,duration "+(end - start));
-            //System.out.println("============================================================================\n\n");
-            //System.out.println("\t\tQuery Execution End at "+end);
-            System.out.println("\t\tTotal Time Taken By Driver :: "+qt+"ms");
-
-            //stmt.close();
-            //System.out.println("Statement is close "+stmt.isClosed());
-            //System.out.println("Result is close "+rs1.isClosed());
+            return qt;
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return 0l;
     }
 
-    public static void gtTimeWithWeb3J(int range){
+    public static long gtTimeWithWeb3J(int range){
         blockRange blnum = getRandomRange(range);
         int minBlock = blnum.rmin;
         int maxBlock = blnum.rmax;
         Long start = System.currentTimeMillis();
         try {
             BigInteger blockHeight = web3jClient.ethBlockNumber().send().getBlockNumber();
-        for (int i = minBlock; i <= maxBlock; i++) {
-            EthBlock block = web3jClient.ethGetBlockByNumber(DefaultBlockParameter.valueOf(new BigInteger(i+"")), true).send();
-            EthBlock.Block blockInfo = block.getBlock();
-            BigInteger blocknumber = blockInfo.getNumber();
-            String hash = blockInfo.getHash();
-            String parenthash = blockInfo.getParentHash();
-            BigInteger nonce = blockInfo.getNonce();
-            String sha3uncles = blockInfo.getSha3Uncles();
-            String logsbloom = blockInfo.getLogsBloom();
-            String transactionsroot = blockInfo.getTransactionsRoot();
-            String stateroot = blockInfo.getStateRoot();
-            String receiptsroot = blockInfo.getReceiptsRoot();
-            String author = blockInfo.getAuthor();
-            String miner = blockInfo.getMiner();
-            String mixhash = blockInfo.getMixHash();
-            BigInteger totaldifficulty = blockInfo.getTotalDifficulty();
-            String extradata = blockInfo.getExtraData();
-            BigInteger size = blockInfo.getSize();
-            BigInteger gaslimit = blockInfo.getGasLimit();
-            BigInteger gasused = blockInfo.getGasUsed();
-            BigInteger timestamp = blockInfo.getTimestamp();
-            List<EthBlock.TransactionResult> transactions = blockInfo.getTransactions();
-            List<String> uncles = blockInfo.getUncles();
-            List<String> sealfields = blockInfo.getSealFields();
-        }
-        Long end = System.currentTimeMillis();
-        System.out.println("\t\tTotal Time Taken By WEB3J :: "+(end - start)+"ms");
+            for (int i = minBlock; i <= maxBlock; i++) {
+                EthBlock block = web3jClient.ethGetBlockByNumber(DefaultBlockParameter.valueOf(new BigInteger(i+"")), true).send();
+                EthBlock.Block blockInfo = block.getBlock();
+                BigInteger blocknumber = blockInfo.getNumber();
+                String hash = blockInfo.getHash();
+                String parenthash = blockInfo.getParentHash();
+                BigInteger nonce = blockInfo.getNonce();
+                String sha3uncles = blockInfo.getSha3Uncles();
+                String logsbloom = blockInfo.getLogsBloom();
+                String transactionsroot = blockInfo.getTransactionsRoot();
+                String stateroot = blockInfo.getStateRoot();
+                String receiptsroot = blockInfo.getReceiptsRoot();
+                String author = blockInfo.getAuthor();
+                String miner = blockInfo.getMiner();
+                String mixhash = blockInfo.getMixHash();
+                BigInteger totaldifficulty = blockInfo.getTotalDifficulty();
+                String extradata = blockInfo.getExtraData();
+                BigInteger size = blockInfo.getSize();
+                BigInteger gaslimit = blockInfo.getGasLimit();
+                BigInteger gasused = blockInfo.getGasUsed();
+                BigInteger timestamp = blockInfo.getTimestamp();
+                List<EthBlock.TransactionResult> transactions = blockInfo.getTransactions();
+                List<String> uncles = blockInfo.getUncles();
+                List<String> sealfields = blockInfo.getSealFields();
+            }
+            Long end = System.currentTimeMillis();
+            return (end - start);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return 0;
     }
-
-
 }
 
 class blockRange {
     public int rmin;
     public int rmax;
-
     public blockRange(int rmin, int rmax) {
         this.rmin = rmin;
         this.rmax = rmax;
