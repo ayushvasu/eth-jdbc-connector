0bb6c327470adde277524f5aa8314628f5f3b930
ayushvasu <ayushvasu98@gmail.com>
2018-07-20 11:16:14 +0530

    Changes in benchmark example to run for webj driver alone

--- eth-jdbc-examples/src/main/java/com/impetus/blkchn/eth/BenchMarkEthDriver.java
+++ eth-jdbc-examples/src/main/java/com/impetus/blkchn/eth/BenchMarkEthDriver.java
@@ -1,5 +1,13 @@
 package com.impetus.blkchn.eth;
 
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.methods.response.EthBlock;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.protocol.ipc.WindowsIpcService;
+
+import java.io.IOException;
+import java.math.BigInteger;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -15,9 +23,14 @@ public class BenchMarkEthDriver {
     static final int numIter = 3;
     static Connection conn = null;
     static Random randomNum = null;
+    static String ipPort = "172.25.41.52:8545";
+    static String httpUrl = "http://"+ipPort;
+    static String url = "jdbc:blkchn:ethereum://"+ipPort;
+    static Web3j web3jClient = null;
+
 
     public static void main(String args[]) throws Exception {
-        String url = "jdbc:blkchn:ethereum://172.25.41.52:8545";
+        web3jClient = Web3j.build(new HttpService(httpUrl));
         String driverClass = "com.impetus.eth.jdbc.EthDriver";
 
         Class.forName(driverClass);
@@ -27,13 +40,16 @@ public class BenchMarkEthDriver {
         for (int ittr = 0; ittr < numIter; ittr++) {
             List<HashMap> queryList = getQueryList();
             for (Object qmap : queryList) {
+                System.out.println("\n\n");
                 for (Object qtype : ((Map) qmap).keySet()) {
-                    System.out.println(qtype + " for iteration " + (ittr + 1));
+                    System.out.println("\n"+qtype + " for iteration " + (ittr + 1));
                     int i = 0;
                     for (Object query : (List) ((HashMap) qmap).get((String) qtype)) {
-                        System.out.println("=========================== Block Range of :: "+blockSize[i++]+" Blocks===================================");
+                        System.out.println("\n\t=========================== Block Range of :: "+blockSize[i]+" Blocks===================================");
                         //System.out.println(blockSize[i++]+"\t\t"+query);
                         runQuery((String) query);
+                        gtTimeWithWeb3J(blockSize[i]);
+                        i++;
                     }
                     System.out.println("\n\n");
                 }
@@ -97,8 +113,8 @@ public class BenchMarkEthDriver {
             Long qt = (end - start);
             //System.out.println("\t\tStart Time :: "+start+" end Time :: "+end+" ,duration "+(end - start));
             //System.out.println("============================================================================\n\n");
-            System.out.println("\t\tQuery Execution End at "+end);
-            System.out.println("Total Time :: "+qt+"ms");
+            //System.out.println("\t\tQuery Execution End at "+end);
+            System.out.println("\t\tTotal Time Taken By Driver :: "+qt+"ms");
 
             //stmt.close();
             //System.out.println("Statement is close "+stmt.isClosed());
@@ -107,6 +123,47 @@ public class BenchMarkEthDriver {
             e.printStackTrace();
         }
     }
+
+    public static void gtTimeWithWeb3J(int range){
+        blockRange blnum = getRandomRange(range);
+        int minBlock = blnum.rmin;
+        int maxBlock = blnum.rmax;
+        Long start = System.currentTimeMillis();
+        try {
+            BigInteger blockHeight = web3jClient.ethBlockNumber().send().getBlockNumber();
+        for (int i = minBlock; i <= maxBlock; i++) {
+            EthBlock block = web3jClient.ethGetBlockByNumber(DefaultBlockParameter.valueOf(new BigInteger(i+"")), true).send();
+            EthBlock.Block blockInfo = block.getBlock();
+            BigInteger blocknumber = blockInfo.getNumber();
+            String hash = blockInfo.getHash();
+            String parenthash = blockInfo.getParentHash();
+            BigInteger nonce = blockInfo.getNonce();
+            String sha3uncles = blockInfo.getSha3Uncles();
+            String logsbloom = blockInfo.getLogsBloom();
+            String transactionsroot = blockInfo.getTransactionsRoot();
+            String stateroot = blockInfo.getStateRoot();
+            String receiptsroot = blockInfo.getReceiptsRoot();
+            String author = blockInfo.getAuthor();
+            String miner = blockInfo.getMiner();
+            String mixhash = blockInfo.getMixHash();
+            BigInteger totaldifficulty = blockInfo.getTotalDifficulty();
+            String extradata = blockInfo.getExtraData();
+            BigInteger size = blockInfo.getSize();
+            BigInteger gaslimit = blockInfo.getGasLimit();
+            BigInteger gasused = blockInfo.getGasUsed();
+            BigInteger timestamp = blockInfo.getTimestamp();
+            List<EthBlock.TransactionResult> transactions = blockInfo.getTransactions();
+            List<String> uncles = blockInfo.getUncles();
+            List<String> sealfields = blockInfo.getSealFields();
+        }
+        Long end = System.currentTimeMillis();
+        System.out.println("\t\tTotal Time Taken By WEB3J :: "+(end - start)+"ms");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
 
 class blockRange {
@@ -118,4 +175,3 @@ class blockRange {
         this.rmax = rmax;
     }
 }
-
